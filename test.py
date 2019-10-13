d468cca5b4a413b9d99e607f8995704a2efdd38f
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2019-10-13 19:42:13 -0400

    Add files via upload
    added restart and exit feature

--- test.py
+++ test.py
@@ -6,13 +6,20 @@ def num_check():
         num = input("give me a number: ")
         check = input("give me another number: ")
         try: # exception handeling
-            if num % check == 0: #check the numbers for even division
+            if int(num) % int(check) == 0: #check the numbers for even division
                 print('This divides evenly. ')
             else:
                 print('This does not divide evenly.')
         except (ZeroDivisionError, UnboundLocalError, NameError, ValueError, TypeError): #exception for these errors
             print('Invalid Operation')
+
+add_active = True
 while True: # initialize the loop
     num_check()
-
+    restart = input("do you want to do another one? - type 'n' to quit: ")
+    if restart == 'n':
+        break
+    else:
+        add_active
+    
 print('Thank you!')
