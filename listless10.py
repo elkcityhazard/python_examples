599ded01ba3f1d678d5d4e2b66abc61f635aa5a1
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2019-10-13 20:35:01 -0400

    a simple list checking example
    Create list, check list for numbers less than 6, used list comprehension, asked for user given number and made a new list with numbers that are less than the number given.

---
+++ listless10.py
@@ -0,0 +1,26 @@
+# createa new list that are less than 6 in the original list
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [] # empty list to hold numbers less than 6
+
+for num in a: # the for loop to get all the numbers less than 6 and append them to the new list
+    if num < 6:
+        b.append(num)
+
+print(b) # print out the smaller list
+
+
+c = [item for item in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if item < 6 ] # list comprehension to check if item in list is less than 6
+
+print(c)
+
+# Ask the user for a number and return a list that contains only elements from
+# the original list a that are smaller than that number given by the user.
+user_number = int(input("Give me a number: "))
+d = [] #empty list to hold all the numbers less than the number given
+
+for num in a: # more for loops
+    if num < user_number: # check if num in a is less than the user given number
+        d.append(num) # append the numbers that are less than the user given number to a new list
+
+print(d) # print out the new list 
