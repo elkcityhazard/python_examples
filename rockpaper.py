8034676881b50b4511b317d35014f13e3a384c34
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2019-10-15 22:25:12 -0400

    Rock, Paper, Scissors
    Added stats, added a few more conditional statements - need to fix check for input to not add invalid moves  to move counter.

--- rockpaper.py
+++ rockpaper.py
@@ -13,18 +13,23 @@ Remember the rules:
 import random
 print('Rock, Paper, Scissors - Presented by Andrew M McCall')
 print("\n\tEnter rock, paper, or scissors and see if you can beat the computer!")
-
 moves = ['rock', 'paper', 'scissor']
 
+wins = 0
+losses = 0
+human_moves = []
+computer_moves =[]
 round = True
 
 while round:
     move = input('\nenter your move: ')
+    human_moves.append(move.lower())
     if move.lower() not in moves:
         print('Invalid Entry!')
         continue
     else:
         computer_move = random.choice(moves)
+        computer_moves.append(computer_move)
 
     print("\nThe computer played: " + str(computer_move))
 
@@ -33,6 +38,7 @@ while round:
         round
     elif move == 'rock' and computer_move == 'scissor':
         print('you win')
+        wins += 1
         again = input("Go Again? (y/n): ")
         if again == 'n':
             break
@@ -40,6 +46,7 @@ while round:
             round
     elif move == 'rock' and computer_move == 'paper':
         print('you lose')
+        losses += 1
         again = input("Go Again? (y/n): ")
         if again == 'n':
             break
@@ -50,6 +57,7 @@ while round:
         round
     elif move == 'paper' and computer_move == 'rock':
         print('You Win!')
+        wins += 1
         again = input("Go Again? (y/n): ")
         if again == 'n':
             break
@@ -57,6 +65,7 @@ while round:
             round
     elif move == 'paper' and computer_move == 'scissor':
         print('You Lose!')
+        losses += 1
         again = input("Go Again? (y/n): ")
         if again == 'n':
             break
@@ -67,6 +76,7 @@ while round:
         round
     elif move == 'scissor' and computer_move == 'rock':
         print('You Lose!')
+        losses += 1
         again = input("Go Again? (y/n): ")
         if again == 'n':
             break
@@ -75,6 +85,7 @@ while round:
     else:
         if move == 'scissor' and computer_move == 'paper':
             print('You Win!')
+            wins += 1
             again = input("Go Again? (y/n): ")
             if again == 'n':
                 break
@@ -82,5 +93,18 @@ while round:
                 round
 
 
-
+total_rounds = wins + losses
 print('Thank you for playing!')
+print("\nYou Won: " +str(wins) + " times.")
+print("\nYou Lost: " + str(losses) + " times.")
+print("\nYou played: " + str(total_rounds) + " rounds.")
+print("\nYou played Rock: " + str(human_moves.count('rock')) + " times.")
+print("\nYou played Paper: " + str(human_moves.count('paper')) + " times.")
+print("\nYou played Scissor: " + str(human_moves.count('scissor')) + " times.")
+print("\nYou made a total of: " + str(len(human_moves)) + " moves.")
+
+# computer results
+print("\nBot played Rock: " + str(computer_moves.count('rock')) + " times.")
+print("\nBot played Paper: " + str(computer_moves.count('paper')) + " times.")
+print("\nBot played Scissor: " + str(computer_moves.count('scissor')) + " times.")
+print("\nBot made a total of: " + str(len(computer_moves)) + " moves.")
